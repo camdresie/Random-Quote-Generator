961df12c97067e28321f4e7e0b14534f924fc052
Cam <cameron.riemensnider@gmail.com>
2020-05-01 21:38:19 -0700

    Quotes file updated

---
+++ quotes.js
@@ -0,0 +1,72 @@
+const quotes = [
+    {
+        quote: "Lady Gaga is my name. If you know me, and you call me Stefani, you don't really know me at all.",
+        source: "Lady Gaga",
+        citation: "Glamour Magazine",
+        year: 2013,
+        tag: "Inspirational"
+    },
+    {
+        quote: "The most important thing is to try and inspire people so that they can be great in whatever they want to do.",
+        source: "Kobe Bryant",
+        citation: "Interview",
+        year: 2005,
+        tag: "Motivational"
+    },
+    {
+        quote: "Trust in dreams, for in them is hidden the gate to eternity.",
+        source: "Kahlil Gibran",
+        citation: "Selected Works",
+        year: 1915,
+        tag: "Motivational"
+    },
+    {
+        quote: "Life is really simple, but we insist on making it complicated.",
+        source: "Confucius",
+        citation: "Journal",
+        year: "5th Century",
+        tag: "Life"
+    },
+    {
+        quote: "Good friends, good books and a sleepy conscience: this is the ideal life.",
+        source: "Mark Twain",
+        citation: "Journal",
+        year: 1888,
+        tag: "Life"
+    },
+    {
+        quote: "Never tell people how to do things. Tell them what to do and they will surprise you with their ingenuity.",
+        source: "George S. Patton",
+        citation: "Interview",
+        year: 1925,
+        tag: "Wisdom"
+    },
+    {
+        quote: "Whatever you do in life, surround yourself with smart people who'll argue with you.",
+        source: "John Wooden",
+        citation: "Interview",
+        year: 1995,
+        tag: 'Inspirational'
+    },
+    {
+        quote: "For beautiful eyes, look for the good in others; for beautiful lips, speak only words of kindness; and for poise, walk with the knowledge that you are never alone.",
+        source: "Audrey Hepburn",
+        citation: "Interview",
+        year: 1975,
+        tag: "Beauty"
+    },
+    {
+        quote: "Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.",
+        source: "Barack Obama",
+        citation: "Speech",
+        year: 2008,
+        tag: "Change"
+    },
+    {
+        quote: "Change your opinions, keep to your principles; change your leaves, keep intact your roots.",
+        source: "Victor Hugo",
+        citation: "Journal",
+        year: 1855,
+        tag: 'Change'
+    }
+];
